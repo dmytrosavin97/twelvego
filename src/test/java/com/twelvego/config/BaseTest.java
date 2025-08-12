1875782395830def74d1cc873fcd01271372d170
dmytrosavin97 <dmytrosavin97@gmail.com>
2025-08-12 15:50:21 -0800

    test.yml

--- src/test/java/com/twelvego/config/BaseTest.java
+++ src/test/java/com/twelvego/config/BaseTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
  
 import java.time.Duration;
@@ -16,7 +17,14 @@ public abstract class BaseTest {
     @BeforeAll
     static void setUpClass() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+ 
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");                 
+        options.addArguments("--headless=new");                
+        options.addArguments("--disable-dev-shm-usage");      
+        options.addArguments("--disable-gpu");                 
+ 
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
